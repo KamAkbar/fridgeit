cb9eabb7a529a7467c66d66da2badc13704eeae0
Akbar <68802342+KamAkbar@users.noreply.github.com>
2024-11-02 22:20:29 +0000

    changed port of app.py

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def upload_barcode():
 
 if __name__ == '__main__':
     setup_database()
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))         # app.run(host="0.0.0.0", port=5001)
